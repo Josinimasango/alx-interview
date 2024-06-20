fef4eff7182ead12ed795cd40e6d245a62520397
Josinimasango <nellyjossydumako@gmail.com>
2024-06-20 12:20:36 -0700

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Script that reads stdin line by line and computes metrics:
+"""
+
+
+def parseLogs():
+    """
+    parsing logs from standard input and calculates
+    the file size and status codes.
+
+    """
+    stdin = __import__('sys').stdin
+    line_counter = 0
+    fileSize = 0
+    status_counts = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for line in stdin:
+            line_counter += 1
+            line = line.split()
+            try:
+                fileSize += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        status_counts[line[-2]] += 1
+                    except KeyError:
+                        status_counts[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if line_counter == 10:
+                report(fileSize, status_counts)
+                line_counter = 0
+        report(fileSize, status_counts)
+    except KeyboardInterrupt as e:
+        report(fileSize, status_counts)
+        raise
+    except BrokenPipeError:
+        # handling BrokenPipeError explicitly
+        pass
+
+
+def report(fileSize, status_counts):
+    """
+    Prints the generated report to standard output
+    """
+    print("File size: {}".format(fileSize))
+    for key, value in sorted(status_counts.items()):
+        print("{}: {}".format(key, value))
+
+
+if __name__ == '__main__':
+    try:
+        parseLogs()
+    except BrokenPipeError:
+        # handling the BrokenPipeError
+        pass
