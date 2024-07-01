af6d0cffec8d6423f70e29ef9f2ced60b25bb787
Josinimasango <nellyjossydumako@gmail.com>
2024-07-01 11:15:29 -0700

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Method that determines if a given data set
+represents a valid UTF-8 encoding.
+
+"""
+
+
+def validUTF8(data):
+    """
+    determines if a given data set represents a valid UTF-8 encoding
+    """
+
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i & 128 == 0:
+                byte_count = 0
+            elif i & 224 == 192:
+                byte_count = 1
+            elif i & 240 == 224:
+                byte_count = 2
+            elif i & 248 == 240:
+                byte_count = 3
+            else:
+                return False
+        else:
+            if i & 192 != 128:
+                return False
+            byte_count -= 1
+
+    return byte_count == 0
