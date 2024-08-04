a2443cd7d7e0f1d2493fc3f14f0dec45b42a5b03
Josinimasango <nellyjossydumako@gmail.com>
2024-08-04 11:37:19 -0700

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
-a function that returns the perimeter of 
-the island described in grid
+returns the perimeter of the island described in grid
 """
 
 
 def island_perimeter(grid):
-    """ calculates the parimeter of the island """
+    """ Returns the parimeter of the island """
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                # Each land cell contributes 4 sides to the perimeter
+                # cell that contributes 4 sides to the perimeter
                 perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
-                    # Subtract 2 if the cell above is also land
+                    # Subtracts 2 if the cell above is also land
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                    # Subtract 2 if the cell on the left is also land
+                    # Subtracts 2 if the cell on the left is also land
                     perimeter -= 2
     return perimeter
